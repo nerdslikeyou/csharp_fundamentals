e691f0c8dd58ad677e2415ea25692631bf2eeba0
Greg Cannon <nerdymind@live.com>
2019-03-07 10:50:18 -0800

    Initial Commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        /// <summary>
+        /// This is XML Documentation
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            //This is a single line comment
+
+            /*
+             * This is a multi-line comment
+             */
+        }
+    }
+}
